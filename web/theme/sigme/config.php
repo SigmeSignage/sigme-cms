603e382e0f833e057732935728af24b749f87816
MarkMiura <miura@sigme.net>
2022-10-28 22:27:55 +0900

    Add theme sigme

---
+++ web/theme/sigme/config.php
@@ -0,0 +1,34 @@
+<?php
+/*
+ * Xibo - Digital Signage - http://www.xibo.org.uk
+ * Copyright (C) 2006-2015 Daniel Garner
+ *
+ * This file is part of Xibo.
+ *
+ * Xibo is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version. 
+ *
+ * Xibo is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with Xibo.  If not, see <http://www.gnu.org/licenses/>.
+ */ 
+
+/*
+ * Theme Configuration Object
+ */
+$config = array(
+        'theme_name' => 'Sigme Theme',
+	'theme_title' => 'Sigme Digital Signage',
+	'app_name' => 'Sigme',
+	'theme_url' => 'https://www.sigme.net',
+	'cms_source_url' => 'https://github.com/SigmeSignage/sigme-cms',
+	'cms_install_url' => 'http://xibo.org.uk/manual/en/install_windows_client.html',
+	'cms_release_notes_url' => 'http://xibo.org.uk/manual/en/release_notes.html',
+	'view_path' => PROJECT_ROOT . '/web/theme/custom/sigme/views'
+	);
